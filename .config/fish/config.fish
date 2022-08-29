bd7d2f3a8eee3dbf3a8cf266ebe5d9a9ef86df09
Gurgen Hayrapetyan <info.gurgen@gmail.com>
2022-08-30 01:11:12 +0400

    Add OTP Pub window to xlib tmux session

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,11 +1,24 @@
 set EDITOR nvim
-set -x DISABLE_AUTO_TITLE true
 set fish_greeting
+set -x DISABLE_AUTO_TITLE true
 
 fish_add_path /opt/homebrew/bin
 fish_add_path $HOME/.nvm/versions/node/v16.16.0/bin
 fish_add_path /opt/local/bin
 fish_add_path /Applications/Postgres.app/Contents/Versions/latest/bin
+
+set --export ANDROID $HOME/Library/Android;
+set --export ANDROID_HOME $ANDROID/sdk;
+set -gx PATH $ANDROID_HOME/tools $PATH;
+set -gx PATH $ANDROID_HOME/tools/bin $PATH;
+set -gx PATH $ANDROID_HOME/platform-tools $PATH;
+set -gx PATH $ANDROID_HOME/emulator $PATH
+
+set --export JAVA_HOME /Applications/Android\ Studio.app/Contents/jre/Contents/Home;
+set --export AWS_PROFILE hearme
+set -gx PATH $JAVA_HOME/bin $PATH;
+
+set -gx SDKROOT /Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX12.3.sdk
 
 alias vim="nvim"
 alias lg="lazygit"
@@ -15,3 +28,9 @@ alias xlib="tmuxp load $HOME/tmuxp-sessions/xlib.yaml"
 alias config="/opt/homebrew/bin/git --git-dir=$HOME/.cfg --work-tree=$HOME"
 
 fish_vi_key_bindings
+
+set -q GHCUP_INSTALL_BASE_PREFIX[1]; or set GHCUP_INSTALL_BASE_PREFIX $HOME ;
+set -gx PATH $HOME/.cabal/bin $PATH /Users/rasjonell/.ghcup/bin # ghcup-env
+
+set -gx tide_elixir_icon 🧪
+set -gx _tide_right_items node elixir aws
